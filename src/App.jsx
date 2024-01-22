9dbb4ad81ae90efaaa315605271337571de15cce
mikigoalie <mikigoalie@seznam.cz>
2024-01-22 05:05:03 +0100

    literally force default color scheme

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <MantineProvider theme={{ colorScheme: 'dark' }}>
+    <MantineProvider  defaultColorScheme="dark" theme={{ colorScheme: 'dark' }}>
       <Container/>
     </MantineProvider>
   )
